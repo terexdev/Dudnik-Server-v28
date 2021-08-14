121c19dd7fa43f9518fb24c21bd64bc1022df9d8
RomashkaTea <80623570+RomashkaTea@users.noreply.github.com>
2021-08-15 00:24:15 +0300

    Add files via upload

---
+++ Logic/EventSlots.py
@@ -0,0 +1,51 @@
+import time
+
+class EventSlots:
+    Timer = 86400
+
+    maps = [
+        # Status = [3 = Nothing, 2 = Star Token, 1 = New Event]
+        {
+            'ID': 40,
+            'Status': 3,
+            'Ended': False,
+            'Modifier': 0
+        },
+
+        {
+            'ID': 228,
+            'Status': 3,
+            'Ended': False,
+            'Modifier': 0
+        },
+
+        {
+            'ID': 146,
+            'Status': 3,
+            'Ended': False,
+            'Modifier': 4
+        },
+
+        {
+            'ID': 24,
+            'Status': 3,
+            'Ended': False,
+            'Modifier': 3
+        },
+
+        {
+            'ID': 269,
+            'Status': 3,
+            'Ended': False,
+            'Modifier': 1
+        },
+
+        {
+            'ID': 290,
+            'Status': 3,
+            'Ended': False,
+            'Modifier': 2
+        }
+
+
+    ]
---
+++ Logic/Quest.py
@@ -0,0 +1,45 @@
+class Quests:
+
+    """
+    << Mission IDs >>
+    1 = Win %Max% battles
+    2 = Win %Max% brawlers
+    3 = Damage %Max%
+    4 = HP %Max%
+
+    Type = ??
+    """
+
+    quests = [
+
+        {
+            'MissionID': 2,
+            'Type': 0,
+            'BrawlerID': 12,
+            'GamemodeID': 11,
+            'Reached': 1,
+            'Max': 3,
+            'Reward': 500,
+            'BrawlPassExclusiveBoolean': 0,
+            'CurrentLVL': 0,
+            'MaxLVL': 0
+        },
+
+        {
+            'MissionID': 1,
+            'Type': 2,
+            'BrawlerID': 11,
+            'GamemodeID': 11,
+            'Reached': 1,
+            'Max': 5,
+            'Reward': 777,
+            'BrawlPassExclusiveBoolean': 0,
+            'CurrentLVL': 5,
+            'MaxLVL': 5
+        },
+
+
+    ]
+
+
+
